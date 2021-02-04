cbf7431bbcd751a017fd9019781fd87cedd78619
Jordan Myer <jordanmyer92@gmail.com>
2021-02-03 21:18:40 -0600

    should print each row

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,3 +3,17 @@ total_profit_loss=0.00
 average_profit_loss=0.00
 greatest_inclrease={}
 greatest_decrease={}
+import csv
+
+file_path = "./Resources/budget_data.csv"
+
+with open(file_path) as csvfile:
+
+    csvreader = csv.reader(csvfile)
+    
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+  
+    for row in csvreader:
+      print(row)
+      
