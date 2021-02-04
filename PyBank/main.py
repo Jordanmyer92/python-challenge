762504d3d706446c918d94863bcee08fa86bda16
Jordan Myer <jordanmyer92@gmail.com>
2021-02-03 21:39:15 -0600

    greatest inclrease amounts

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,9 +1,11 @@
+
+import csv
 total_months=0
 total_profit_loss=0.00
 average_profit_loss=0.00
-greatest_inclrease={}
+greatest_increase={"date":"amount":0}
 greatest_decrease={}
-import csv
+
 
 file_path = "./Resources/budget_data.csv"
 
@@ -16,8 +18,12 @@ with open(file_path) as csvfile:
   
     for row in csvreader:
         total_months=total_months+1
-         print(row)
-      
+        date=row[0]
+        profit=float(row[1])
+        if(profit >greatest_increase["amount"]  ):
+           greatest_increase["date"]=date 
+           greatest_increase["amount"]=profit
 
 print("Financial Analysis")
 print(f"Total Months:{total_months}")
+print(f"Greatest Increase in Profits: {greatest_increase["date"]}(${greatest_increase["amount]"})
