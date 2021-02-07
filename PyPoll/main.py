5c576de4bcbd9794829615c88605b1901e9e596a
Jordan Myer <jordanmyer92@gmail.com>
2021-02-06 23:58:09 -0600

    added voter ID=row[0]

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -22,26 +22,26 @@ with open(file_path) as csvfile:
    print(f"CSV Header: {csv_header}")
   
    for row in csvreader:
-      #total number of months in the data set
-      total_months=total_months+1
-      date=row[0]
-      profit=float(row[1])
+      #total number of votes cast
+      total_votes=total_votes+1
+      Voter_ID=row[0]
+      =float(row[1])
 
       #net total amount of "profit/loses" over the enite period
-      total_profit_loss= (profit + 1)
+      #total_profit_loss= (profit + 1)
 
       #calculate the changes in "profit/loses" over the enitre period then find the average of those changes
-      average_profit_loss =(profit+1) // (total_months+1)
+      #average_profit_loss =(profit+1) // (total_months+1)
 
       # greatest increase in profits (date and amount) over the entire period
-      if (profit > greatest_increase["amount"]):
-            greatest_increase["date"] = date 
-            greatest_increase["amount"] = profit
+     # if (profit > greatest_increase["amount"]):
+       #     greatest_increase["date"] = date 
+        #    greatest_increase["amount"] = profit
 
       # greatest decrease in loses (date and amount) over the entire period 
-      if(profit < greatest_decrease["amount"]):
-            greatest_decrease["date"]=date 
-            greatest_decrease["amount"]=profit   
+     # if(profit < greatest_decrease["amount"]):
+         #   greatest_decrease["date"]=date 
+         #   greatest_decrease["amount"]=profit   
             
        
       
@@ -53,7 +53,7 @@ with open(file_path) as csvfile:
 print("Election Results")
 print("-------------------")
 print(f"Total Votes: {total_votes}")
-print(f"Net Total Proft/Loss: ${total_profit_loss}")
-print(f"Average Change: ${average_profit_loss}")
-print(f"Greatest Increase in Profits: {greatest_increase['date']} (${greatest_increase['amount']})")
-print(f"Greatest Decrease in Profits: {greatest_decrease['date']} (${greatest_decrease['amount']})")
+#print(f"Net Total Proft/Loss: ${total_profit_loss}")
+#print(f"Average Change: ${average_profit_loss}")
+#print(f"Greatest Increase in Profits: {greatest_increase['date']} (${greatest_increase['amount']})")
+#print(f"Greatest Decrease in Profits: {greatest_decrease['date']} (${greatest_decrease['amount']})")
