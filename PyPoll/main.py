56ee9d1a745a39e07fc2e5833858c4449beca252
Jordan Myer <jordanmyer92@gmail.com>
2021-02-07 10:21:55 -0600

    made percent of votes variable

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,9 +3,9 @@ import csv
 #total number of votes cast
 total_votes = 0
 #complete list of candidates who received vote
-
+candidates_list = {"Candidate":""}
 #percentage of votes each candidate won
-
+percent_of_votes = 0.00
 # total number of votes each candidate won
 
 # winner ofelection based on votes
@@ -28,21 +28,17 @@ with open(file_path) as csvfile:
       County=row[1]
       Candidate=row[2]
 
-      #net total amount of "profit/loses" over the enite period
-      #total_profit_loss= (profit + 1)
+      #complete list of candidates who received vote
+      
 
-      #calculate the changes in "profit/loses" over the enitre period then find the average of those changes
-      #average_profit_loss =(profit+1) // (total_months+1)
+      #percentage of votes each candidate won
+      #percent_of_votes = (Candidate+1) // (total_votes+1)
 
-      # greatest increase in profits (date and amount) over the entire period
-     # if (profit > greatest_increase["amount"]):
-       #     greatest_increase["date"] = date 
-        #    greatest_increase["amount"] = profit
+      # total number of votes each candidate won
+   
 
-      # greatest decrease in loses (date and amount) over the entire period 
-     # if(profit < greatest_decrease["amount"]):
-         #   greatest_decrease["date"]=date 
-         #   greatest_decrease["amount"]=profit   
+      # winner ofelection based on votes
+     
             
        
       
@@ -54,7 +50,9 @@ with open(file_path) as csvfile:
 print("Election Results")
 print("-------------------")
 print(f"Total Votes: {total_votes}")
-#print(f"Net Total Proft/Loss: ${total_profit_loss}")
-#print(f"Average Change: ${average_profit_loss}")
+print("-------------------")
+print(f"Candidate: {candidates_list['Candidate']} ") 
 #print(f"Greatest Increase in Profits: {greatest_increase['date']} (${greatest_increase['amount']})")
 #print(f"Greatest Decrease in Profits: {greatest_decrease['date']} (${greatest_decrease['amount']})")
+
+
